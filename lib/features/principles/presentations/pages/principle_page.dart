152d3d6dc18c9bcc5f8c229865779762f026b99f
onlin59 <taez.khamrangsi@gmail.com>
2025-05-27 12:46:56 +0000

    change page title as group name

--- lib/features/principles/presentations/pages/principle_page.dart
+++ lib/features/principles/presentations/pages/principle_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:real_estate_helper/di/injection.dart';
+import 'package:real_estate_helper/features/principle_group/domain/entities/group.dart';
 import 'package:real_estate_helper/features/principles/presentations/bloc/principle_bloc.dart';
 import 'package:real_estate_helper/features/principles/presentations/widgets/principle_card.dart';
 import 'package:real_estate_helper/features/principles/presentations/widgets/principle_dialog.dart';
 import 'package:real_estate_helper/features/principles/domain/entities/principle.dart';
 
 class PrinciplePage extends StatelessWidget {
-  final String groupId;
+  final Group group;
 
-  const PrinciplePage({required this.groupId, super.key});
+  const PrinciplePage({required this.group, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Principles')),
+      appBar: AppBar(title: Text(group.name)),
       body: SafeArea(
         child: BlocBuilder<PrincipleBloc, PrincipleState>(
           builder: (context, state) {
@@ -60,7 +61,7 @@ class PrinciplePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => showAddPrincipleDialog(context, groupId),
+        onPressed: () => showAddPrincipleDialog(context, group.id),
         tooltip: "Add Principle",
         child: const Icon(Icons.add),
       ),
